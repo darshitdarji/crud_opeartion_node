70c99bc66ebdef6dc90834d3cb916d58aba2872c
darshitdarji <darjidarshit2103@gmail.com>
2025-07-06 15:19:55 +0530

    changes successfully

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ mongoose
 
 app.post("/users", async (req, res) => {
   try {
-     user = await User.create(req.body);
+   const  user = await User.create(req.body);
     res.status(201).json(user);
   } catch (err) {
     res.status(400).json({ message: err.message });
